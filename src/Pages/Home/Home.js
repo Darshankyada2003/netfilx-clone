115535fbc5c1ade90aba43d9a270b579896386b1
darshan kyada <darshankyada02@gmail.com>
2024-12-09 16:16:45 +0530

    image slider fetch to api data and etc changes

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -13,29 +13,29 @@ import axios from 'axios'
 
 const Home = ({ settings }) => {
 
-    const sideshowimg = [
-        sideshow1,
-        sideshow2,
-        sideshow3,
-        sideshow4
-    ]
+    // const sideshowimg = [
+    //     sideshow1,
+    //     sideshow2,
+    //     sideshow3,
+    //     sideshow4
+    // ]
 
     const [current, setCurrent] = useState(0);
     const [fade, setFade] = useState(false);
     const [category, setCategory] = useState([]);
+    const [sliderImage, setSliderImage] = useState([]);
 
     useEffect(() => {
+        setFade(true);
         const interval = setInterval(() => {
             setFade(false);
             setTimeout(() => {
-                setCurrent((prevIndex) => (prevIndex + 1) % sideshowimg.length);
+                setCurrent((prevIndex) => (prevIndex + 1) % sliderImage.length);
                 setFade(true);
-
             }, 1000);
-
         }, 4000);
         return () => clearInterval(interval);
-    }, [sideshowimg.length]);
+    }, [sliderImage.length]);
 
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_BASE_URL}/home`)
@@ -75,22 +75,64 @@ const Home = ({ settings }) => {
         };
     }, [category])
 
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_BASE_URL}/topImages`)
+            .then(res => {
+                if (res.data.status) {
+                    setSliderImage(res.data.data);
+                    console.log(res.data.data);
+                }
+            })
+            .catch(err => {
+                console.log(err, "error");
+
+            });
+    }, [])
+
     return (
         <div className='Home'>
             <Navbar settings={settings} />
             <div className='hero'>
-                <img src={sideshowimg[current]} alt='' className={`banner-img ${fade ? 'fade' : 'fade-out'}`} />
-                <div className='hero-caption'>
-                    <img src={hero_title} alt='' className='caption-img' />
-                    <p>Discovering hit ties to a secert ancient order, a young
-                        man living in modern Istanbul embark on a quest to save the
-                        city from an immortal enemy.</p>
-                    <br />
-                    <div className='hero-btn'>
-                        <button className='btn'><img src={play_icon} alt='' />Play</button>
-                        <button className='btn dark-btn'><img src={info_icon} alt='' />More Info</button>
-                    </div>
-                </div>
+                {sliderImage &&
+                    sliderImage.map((item, index) => (
+                        <div key={index} className=''>
+                            {index === current && (
+                                <>
+                                    {item.isImage ? (
+                                        <img
+                                            src={item.image}
+                                            alt=''
+                                            className={`banner-img ${fade ? 'fade' : 'fade-out'}`} />
+                                    ) : (
+                                        <video loop autoPlay muted
+                                            className={`banner-img ${fade ? 'fade' : 'fade-out'}`} >
+                                            <source src={item.image} alt='' type='video/mp4' />
+                                        </video>
+                                    )}
+                                    <div className='hero-caption'>
+                                        <h2 className={`caption-img ${fade ? 'fade' : 'fade-out'}`}>{item.title}</h2>
+                                        <div className={`caption-row ${fade ? 'fade' : 'fade-out'}`}>
+                                            <p className={`caption-item ${fade ? 'fade' : 'fade-out'}`}>{item.release_date}</p>
+                                            <p className={`caption-item ${fade ? 'fade' : 'fade-out'}`}>{item.language}</p>
+                                            <p className={`caption-item ${fade ? 'fade' : 'fade-out'}`}>{item.duration}</p>
+                                        </div>
+                                        <br />
+                                        <p className={`caption-img ${fade ? 'fade' : 'fade-out'}`}>{item.description}</p>
+                                        <p className={`caption-img ${fade ? 'fade' : 'fade-out'}`}>{item.category.join(" | ")}</p>
+                                        <br />
+                                        <div className='hero-btn'>
+                                            {item.isPlay && (
+                                                <button className='btn'><img src={play_icon} alt='' />Play</button>
+                                            )}
+                                            {item.isMoreInfo && (<button className='btn dark-btn'><img src={info_icon} alt='' />More Info</button>)}
+                                        </div>
+                                    </div>
+
+                                </>
+                            )}
+                        </div>
+                    ))
+                }
             </div>
             <div className='title-card-section'>
                 {category.length > 0 &&
